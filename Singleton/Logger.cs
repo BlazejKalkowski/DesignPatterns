92c3f36e6b08c941e3facc662feacd5b0423a2b7
BlazejKalkowski <blazej.kolek@gmail.com>
2025-01-24 13:48:05 +0100

    Add Singleton Pattern

---
+++ Singleton/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Logger
+    {
+        private static Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+        private static readonly object _lock = new object();
+        private Logger() { }
+        public static Logger Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        public void LogMessage(DateTime date, string message)
+        {
+            String inputText = $"Log create on date : {date} with message : {message} : {Environment.NewLine}";
+
+            lock (_lock)
+            {
+                File.AppendAllText("log.txt", inputText);
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+
+
+var tasks = new Task[5];
+
+for (int i = 0; i < tasks.Length; i++)
+{
+    int taskNumber = i + 1;
+    tasks[i] = Task.Run(() => LogMessage(taskNumber));
+}
+
+await Task.WhenAll(tasks);
+
+Console.ReadKey();
+
+static void LogMessage(int taskNumber)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Logger.Instance.LogMessage(DateTime.Now, taskNumber.ToString());
+        Task.Delay(1000);
+    }
+}
